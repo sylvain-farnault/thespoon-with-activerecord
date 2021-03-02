910feec1c3a49aa91449ec3cf7a745f0e6c51ca6
Sylvain FARNAULT <sylvain.farnault@gmail.com>
2021-03-02 12:01:13 +0100

    Restaurant CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all restaurants
+  # get "restaurants", to: "restaurants#index"
+  # # See details about one restaurant
+  # get "restaurants/:id", to: 'restaurants#show'
+  # # Create a restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # Update a restaurant
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Destroy a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
+
 end
